48b6abdf9dd3eb40c82215dfe74b91b89f3c18ef
Alistair Crook <acrook@acrook.co.uk>
2021-05-25 19:30:05 +0100

    Minimum working product

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, world");
+import { Client } from "discord.js";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client?.user?.tag}!`);
+  });
+
+  
+client.on('message', msg => {
+    if (msg.content === 'hello') {
+      msg.reply('hello!');
+    }
+  });
+
+client.login(process.env.DISCORD_TOKEN);
