08489fd1ae101d38e65a9c427b6147d1ab56b940
Alistair Crook <acrook@acrook.co.uk>
2021-05-25 23:04:37 +0100

    Static-ify onMessage

--- src/discord.ts
+++ src/discord.ts
@@ -15,7 +15,26 @@ export class DiscordClient {
         this._client.on('ready', () => {
             console.log(`Logged in as ${this._client?.user?.tag}!`);
         });
-        this._client.on('message', this.onMessage);
+        this._client.on('message', (message) => DiscordClient.onMessage(this, message));
+    }
+
+    static onMessage(client: DiscordClient, message: Message): void {
+        const content = message.content.toLowerCase();
+        if (content) {
+            for (let item of content.match(ITEM_MATCHER) || []) {
+                client.findItem(item, message);
+            }
+
+            if (content.toLowerCase().includes("good bot")) message.reply(HEART);
+            if (content.toLowerCase().includes("bad bot")) message.reply(RUDE);
+
+            if (!content.startsWith(COMMAND_INVOCATION) || message.author.bot) return;
+            const commandString = (content.split(" ")[0].substring(COMMAND_INVOCATION.length));
+            const command = client._commands.get(commandString);
+            if (command) command(message);
+            else message.channel.send("Command not recognised.");
+            
+        }
     }
 
     client(): Client {
@@ -24,25 +43,6 @@ export class DiscordClient {
 
     addCommand(command: string, functionToCall: (message: Message) => void): void {
         this._commands.set(command.toLowerCase(), functionToCall);
-    }
-
-    onMessage(message: Message): void {
-        const content = message.content.toLowerCase();
-        if (content) {
-            for (let item of content.match(ITEM_MATCHER) || []) {
-                this.findItem(item, message);
-            }
-
-            if (content.toLowerCase().includes("good bot")) message.reply(HEART);
-            if (content.toLowerCase().includes("bad bot")) message.reply(RUDE);
-
-            if (!content.startsWith(COMMAND_INVOCATION) || message.author.bot) return;
-            const commandString = (content.split(" ")[0].substring(COMMAND_INVOCATION.length));
-            const command = this._commands.get(commandString);
-            if (command) command(message);
-            else message.channel.send("Command not recognised.");
-            
-        }
     }
 
     findItem(item: string, message: Message) {
