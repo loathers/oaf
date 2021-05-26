0ce70497cc4d1c2dca23b97170ca0a16142a7a72
Alistair Crook <acrook@acrook.co.uk>
2021-05-26 09:04:51 +0100

    Remove webserver, add rudimentary app.yml

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import * as dotenv from "dotenv";
 import { DiscordClient } from "./discord";
 import { attachUtils } from "./utils";
-import { startWebserver } from "./webserver";
 
 dotenv.config({ path: __dirname+'/.env' });
 
-
-const WEBSERVER_PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN || "";
-
-startWebserver(WEBSERVER_PORT);
 
 const client = new DiscordClient();
 attachUtils(client);
