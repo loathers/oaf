b941a1e268caee509368073edcd458c35483e9e9
Alistair Crook <acrook@acrook.co.uk>
2021-05-26 18:27:57 +0100

    Tons of logging

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,16 @@ import { VariableManager } from "./variables";
 
 
 const varManager = new VariableManager();
+console.log("Initialised variable manager")
 
 const client = new DiscordClient();
+console.log("Initialised discord client")
 attachUtils(client);
+console.log("Attached utility commands")
 
+console.log("Starting discord client")
 varManager.get("DISCORD_TOKEN").then((discord_token) => {
+        console.log(`Retrieved discord token ${discord_token}`)
         client.start(discord_token || "");
     }
 )
--- src/variables.ts
+++ src/variables.ts
@@ -2,14 +2,16 @@
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: __dirname+'/.env' });
 
 export class VariableManager {
     private _variables: Map<string, string | undefined> = new Map();
+    private _dotEnvConfig: dotenv.DotenvConfigOutput;
     private _googleSecretClient: SecretManagerServiceClient;
 
     constructor() {
         this._googleSecretClient = new SecretManagerServiceClient();
+        this._dotEnvConfig = dotenv.config({ path: __dirname+'/.env' });
+        console.log("Variable manager started");
     }
 
     async getSecret(identifier: string): Promise<string | undefined> {
@@ -17,6 +19,11 @@ export class VariableManager {
             const secret = await this._googleSecretClient.accessSecretVersion({
                 name: `${process.env.SECRET_PATH}/${identifier}/latest`,
             })
+            console.log("Secret request returned.");
+            console.log(secret);
+            console.log(secret[0]);
+            console.log(secret[0].payload);
+            console.log(secret[0].payload?.data);
             if (secret[0].payload) {
                 return secret[0].payload.data?.toString();
             }
@@ -27,10 +34,19 @@ export class VariableManager {
     }
 
     async get(identifier: string): Promise<string | undefined> {
+        console.log(`Looking for variable with identifier ${identifier}`);
         if (!this._variables.has(identifier)) {
-            this._variables.set(identifier, (await this.getSecret(identifier)) || process.env[identifier])
+            console.log(`Identifier ${identifier} not previously seen.`);
+            if (process.env[identifier]) {
+                console.log(`Environment variable ${identifier} found.`);
+                this._variables.set(identifier, process.env[identifier])
+            }
+            else {
+                console.log(`Looking for secret ${process.env.SECRET_PATH}/${identifier}/latest`);
+                this._variables.set(identifier, (await this.getSecret(identifier)))
+            }
         }
-
+        console.log(`Returning ${this._variables.get(identifier)} for variable ${identifier}.`)
         return this._variables.get(identifier) || undefined;
     }
 
