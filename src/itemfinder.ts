18978de90327b18640e1873ba46d26e746ba71b1
Alistair Crook <acrook@acrook.co.uk>
2021-05-26 23:31:46 +0100

    replace all not just some

--- src/itemfinder.ts
+++ src/itemfinder.ts
@@ -13,7 +13,7 @@ export class ItemFinder {
     }
 
     async findName(searchTerm: string): Promise<string | undefined> {
-        const searchTermCrushed = searchTerm.replace(" ", "_").replace(/[^A-Za-z0-9_\-:\/]/g, '');
+        const searchTermCrushed = searchTerm.replaceAll(" ", "_").replace(/[^A-Za-z0-9_\-:\/]/g, '');
         if (this._nameMap.has(searchTermCrushed)) return this._nameMap.get(searchTermCrushed);
         try {
             const wikiResponse = await get(`http://kol.coldfront.net/thekolwiki/index.php?search=${searchTermCrushed}`);
