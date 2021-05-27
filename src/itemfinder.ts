ab4393d811e9894d895c18decfdc67b288225867
Alistair Crook <acrook@acrook.co.uk>
2021-05-27 18:08:11 +0100

    Only return good wiki searches

--- src/itemfinder.ts
+++ src/itemfinder.ts
@@ -13,11 +13,12 @@ export class ItemFinder {
     }
 
     async findName(searchTerm: string): Promise<string | undefined> {
-        const searchTermCrushed = searchTerm.replace(/\s/g, '_').replace(/[^A-Za-z0-9_\-:\/]/g, '');
+        const searchTermCrushed = searchTerm.replace(/\s/g, '_').replace(/[^A-Za-z0-9_\-:\/\(\)\,\.\!\?\']/g, '');
         if (this._nameMap.has(searchTermCrushed)) return this._nameMap.get(searchTermCrushed);
         try {
             const wikiResponse = await get(`http://kol.coldfront.net/thekolwiki/index.php?search=${searchTermCrushed}`);
-            return(wikiResponse.request.res.responseUrl);
+            const responseUrl = String(wikiResponse.request.res.responseUrl);
+            if (!responseUrl.indexOf("index.php?search=")) return responseUrl;
         }
         catch {}
         return undefined;
