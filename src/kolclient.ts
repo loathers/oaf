829796affbb6b165d9f3f7cbb06500b94abcc555
Alistair Crook <acrook@acrook.co.uk>
2021-05-30 05:13:35 +0100

    Fix furry pill/other <br /> line breaks

--- src/kolclient.ts
+++ src/kolclient.ts
@@ -130,6 +130,6 @@ export class KOLClient {
     }
 
     private sanitiseBlueText(blueText: string): string {
-        return decode(blueText.replace(/\r/g, "").replace(/\r/g, "").replace(/(<p><\/p>)|(<br>)/g, "\n").replace(/<[^<>]+>/g, "").replace(/(\n+)/g, "\n").replace(/(\n)+$/, ""))
+        return decode(blueText.replace(/\r/g, "").replace(/\r/g, "").replace(/(<p><\/p>)|(<br>)|(<br \/>)/g, "\n").replace(/<[^<>]+>/g, "").replace(/(\n+)/g, "\n").replace(/(\n)+$/, ""))
     }
 }
