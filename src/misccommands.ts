37a6ed2196f4a1421c41215f62dccd79434a227b
Alistair Crook <acrook@acrook.co.uk>
2021-05-31 20:46:49 +0100

    Add purge command

--- src/misccommands.ts
+++ src/misccommands.ts
@@ -1,13 +1,18 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
+import { EIGHTBALL_RESPONSES } from "./constants";
 import { DiscordClient } from "./discord";
 
 export function attachMiscCommands(client: DiscordClient) {
   client.addCommand("8ball", eightBall);
   client.addCommand("roll", roll);
+  client.addCommand(
+    "purge",
+    async (message) => await purge(message.channel as TextChannel, client)
+  );
 }
 
 function eightBall(message: Message): void {
-  message.channel.send(eightBallRolls[Math.floor(Math.random() * eightBallRolls.length)]);
+  message.channel.send(EIGHTBALL_RESPONSES[Math.floor(Math.random() * EIGHTBALL_RESPONSES.length)]);
 }
 
 function roll(message: Message, args: string[]): void {
@@ -43,39 +48,11 @@ function roll(message: Message, args: string[]): void {
   }
 }
 
-const eightBallRolls = [
-  "It is certain.",
-  "It is decidedly so.",
-  "Without a doubt.",
-  "Yes - definitely.",
-  "You may rely on it.",
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Yes.",
-  "Signs point to yes.",
-  "Reply hazy, try again.",
-  "Ask again later.",
-  "Better not tell you now.",
-  "Cannot predict now.",
-  "Concentrate and ask again.",
-  "Don't count on it.",
-  "My reply is no.",
-  "My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful.",
-  "If CDM has a free moment, sure.",
-  "Why not?",
-  "How am I meant to know?",
-  "I guess???",
-  "...you realise that this is just a random choice from a list of strings, right?",
-  "I have literally no way to tell.",
-  "Ping Bobson, he probably knows",
-  "Check the wiki, answer's probably in there somewhere.",
-  "The wiki has the answer.",
-  "The wiki has the answer, but it's wrong.",
-  "I've not finished spading the answer to that question yet.",
-  "The devs know, go pester them instead of me.",
-  "INSUFFICIENT DATA FOR MEANINGFUL ANSWER",
-  "THERE IS AS YET INSUFFICIENT DATA FOR A MEANINGFUL ANSWER",
-];
+async function purge(channel: TextChannel, client: DiscordClient): Promise<void> {
+  for (let message of (await channel.messages.fetch()).array()) {
+    if (message.author.id === client?.client()?.user?.id) {
+      await message.delete();
+      return;
+    }
+  }
+}
