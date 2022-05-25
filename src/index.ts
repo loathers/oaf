dbcdfb09e41d9a4bca3ac8f5e04d2923384fff89
Alistair Crook <acrook@acrook.co.uk>
2022-05-25 09:57:27 +0100

    Move setup to function so we can async/await

--- src/index.ts
+++ src/index.ts
@@ -8,42 +8,52 @@ import { attachKoLCommands } from "./kolcommands";
 import { Pool } from "pg";
 import { migrate } from "postgres-migrations";
 
+async function performSetup(): Promise<DiscordClient> {
+  console.log("Creating KOL client.");
+  const kolClient = new KOLClient();
+
+  console.log("Creating database client pool.");
+  const databaseClientPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  console.log("Creating wiki searcher.");
+  const wikiSearcher = new WikiSearcher(kolClient);
+
+  console.log("Migrating database.");
+  await migrate({ client: databaseClientPool }, "./migrations");
+
+  console.log("Syncing database.");
+  await syncToDatabase(databaseClientPool);
+
+  console.log("Downloading mafia data.");
+  await wikiSearcher.downloadMafiaData();
+  console.log("All mafia data downloaded.");
+
+  console.log("Creating discord client.");
+  const discordClient = new DiscordClient(wikiSearcher);
+
+  console.log("Attaching misc commands.");
+  attachMiscCommands(discordClient);
+
+  console.log("Attaching kol commands.");
+  attachKoLCommands(discordClient);
+
+  console.log("Attaching clan commands.");
+  attachClanCommands(discordClient, kolClient, databaseClientPool);
+
+  console.log("Attaching wiki commands.");
+  discordClient.attachMetaBotCommands();
+
+  return discordClient;
+}
+
 dotenv.config({ path: __dirname + "/.env" });
 
-console.log("Creating KOL client.");
-const kolClient = new KOLClient();
-
-console.log("Creating database client pool.");
-const databaseClientPool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-console.log("Creating wiki searcher.");
-const wikiSearcher = new WikiSearcher(kolClient);
-
-console.log("Migrating database.");
-migrate({ client: databaseClientPool }, "./migrations").then(() => {
-  console.log("Syncing database.");
-  syncToDatabase(databaseClientPool).then(() => {
-    console.log("Downloading mafia data.");
-    wikiSearcher.downloadMafiaData().then(() => {
-      console.log("All mafia data downloaded.");
-      console.log("Creating discord client.");
-      const discordClient = new DiscordClient(wikiSearcher);
-      console.log("Attaching misc commands.");
-      attachMiscCommands(discordClient);
-      console.log("Attaching kol commands.");
-      attachKoLCommands(discordClient);
-      console.log("Attaching clan commands.");
-      attachClanCommands(discordClient, kolClient, databaseClientPool);
-      console.log("Attaching wiki commands.");
-      discordClient.attachMetaBotCommands();
-
-      console.log("Starting bot.");
-      discordClient.start();
-    });
-  });
+performSetup().then((discordClient) => {
+  console.log("Starting bot.");
+  discordClient.start();
 });
