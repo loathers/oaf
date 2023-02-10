20dbb4dc354489429fdba8f6a7d79e9a741df98d
Sam Gaus <sam@gaus.co.uk>
2023-02-10 12:17:25 +0000

    Simplify rescan command

--- src/commands/wiki/rescan.ts
+++ src/commands/wiki/rescan.ts
@@ -3,14 +3,14 @@ import { CommandInteraction } from "discord.js";
 import { WikiSearcher } from "../../wikisearch";
 import { Command } from "../type";
 
-async function reloadMafiaData(
-  interaction: CommandInteraction,
-  wikiSearcher: WikiSearcher
-): Promise<void> {
+async function rescanCommand(interaction: CommandInteraction, wikiSearcher: WikiSearcher) {
   await interaction.deferReply();
-  if (await wikiSearcher.conditionallyReloadMafiaData())
-    interaction.editReply("Information reloaded from KoLMafia Github data files.");
-  else interaction.editReply("Information reloaded too recently, try again later.");
+
+  if (await wikiSearcher.conditionallyReloadMafiaData()) {
+    return interaction.editReply("Information reloaded from KoLMafia Github data files.");
+  }
+
+  return interaction.editReply("Information reloaded too recently, try again later.");
 }
 
 const command: Command = {
@@ -18,7 +18,7 @@ const command: Command = {
     discordClient.attachCommand(
       "rescan",
       [],
-      async (interaction: CommandInteraction) => await reloadMafiaData(interaction, wikiSearcher),
+      async (interaction: CommandInteraction) => await rescanCommand(interaction, wikiSearcher),
       "Reload OAF's in-game information from mafia's datafiles."
     ),
 };
