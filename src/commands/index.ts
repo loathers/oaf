6bc2e55716cb8702b515fc351de0b3926275ab9a
Sam Gaus <sam@gaus.co.uk>
2023-02-17 16:48:20 +0000

    Remove clan mass import

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,5 @@
-import clan from "./clan";
 import kol from "./kol";
 import misc from "./misc";
 import wiki from "./wiki";
 
-export default { ...clan, ...kol, ...misc, ...wiki };
+export default { ...kol, ...misc, ...wiki };
