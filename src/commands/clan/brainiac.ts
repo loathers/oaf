59640e6709296509a0a945e0ca4883fe6dea6834
Sam Gaus <sam@gaus.co.uk>
2023-02-19 09:36:28 +0000

    Combine some toggle commands

--- src/commands/clan/brainiac.ts
+++ src/commands/clan/brainiac.ts
@@ -12,31 +12,36 @@ export const data = new SlashCommandBuilder()
       .setName("player")
       .setDescription("The player to set as always available for brain draining.")
       .setRequired(true)
+  )
+  .addBooleanOption((option) =>
+    option
+      .setName("available")
+      .setDescription("Whether the player is available or not (default: true)")
+      .setRequired(false)
   );
 
 export async function execute(interaction: CommandInteraction) {
   const username = interaction.options.getString("player", true).toLowerCase();
+  const available = interaction.options.getBoolean("available", false) ?? true;
 
   await interaction.deferReply();
 
   await pool.query(
-    "INSERT INTO players (username, brainiac) VALUES ($1, TRUE) ON CONFLICT (username) DO UPDATE SET brainiac = TRUE;",
-    [username]
+    "INSERT INTO players (username, brainiac) VALUES ($1, $2) ON CONFLICT (username) DO UPDATE SET brainiac = $2;",
+    [username, available]
   );
 
   if (clanState.killMap.has(username)) {
-    clanState.killMap.get(username)!.brainiac = true;
+    clanState.killMap.get(username)!.brainiac = available;
   } else {
     clanState.killMap.set(username, {
       kills: 0,
       skills: 0,
-      brainiac: true,
+      brainiac: available,
     });
   }
 
   await interaction.editReply(
-    `Added user "${username}" to the list of players always available to help with skills.`
+    `${available ? "Added" : "Removed"} user "${username}" ${available ? "to" : "from"} the list of players always available to help with skills.`
   );
-
-  return;
 }
--- src/commands/clan/done.ts
+++ src/commands/clan/done.ts
@@ -5,21 +5,30 @@ import { pool } from "../../db";
 
 export const data = new SlashCommandBuilder()
   .setName("done")
-  .setDescription("Set a player as done with Dreadsylvania skills.")
+  .setDescription("Set whether a player is done with Dreadsylvania skills.")
   .addStringOption((option) =>
     option
       .setName("player")
       .setDescription("The player to set as done with skills.")
       .setRequired(true)
+  )
+  .addBooleanOption((option) =>
+    option
+      .setName("done")
+      .setDescription("Whether the player is done or not (default: true)")
+      .setRequired(false)
   );
 
 export async function execute(interaction: CommandInteraction) {
   const username = interaction.options.getString("player", true).toLowerCase();
+  const done = interaction.options.getBoolean("done", false) ?? true;
+
   await interaction.deferReply();
+
   await pool.query(
-    "INSERT INTO players (username, done_with_skills) VALUES ($1, TRUE) ON CONFLICT (username) DO UPDATE SET done_with_skills = TRUE;",
-    [username]
+    "INSERT INTO players (username, done_with_skills) VALUES ($1, $2) ON CONFLICT (username) DO UPDATE SET done_with_skills = $2;",
+    [username, done]
   );
-  interaction.editReply(`Added user "${username}" to the list of players done with skills.`);
-  return;
+
+  await interaction.editReply(`${done ? "Added" : "Removed"} user "${username}" ${done ? "to" : "from"} the list of players done with skills.`);
 }
