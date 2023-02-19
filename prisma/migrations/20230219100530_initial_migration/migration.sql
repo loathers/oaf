2f8e906b1cfd08c568a82490bba688c16d84e369
Sam Gaus <sam@gaus.co.uk>
2023-02-19 10:34:34 +0000

    Remove table creation from initial migrations temporarily so that we can do the changeover on production

--- prisma/migrations/20230219100530_initial_migration/migration.sql
+++ prisma/migrations/20230219100530_initial_migration/migration.sql
@@ -8,37 +8,37 @@ CREATE TABLE "migrations" (
     CONSTRAINT "migrations_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "players" (
-    "username" VARCHAR(31) NOT NULL,
-    "kills" INTEGER NOT NULL DEFAULT 0,
-    "skills" SMALLINT NOT NULL DEFAULT 0,
-    "done_with_skills" BOOLEAN NOT NULL DEFAULT false,
-    "user_id" VARCHAR(8),
-    "brainiac" BOOLEAN NOT NULL DEFAULT false,
+-- -- CreateTable
+-- CREATE TABLE "players" (
+--     "username" VARCHAR(31) NOT NULL,
+--     "kills" INTEGER NOT NULL DEFAULT 0,
+--     "skills" SMALLINT NOT NULL DEFAULT 0,
+--     "done_with_skills" BOOLEAN NOT NULL DEFAULT false,
+--     "user_id" VARCHAR(8),
+--     "brainiac" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "players_pkey" PRIMARY KEY ("username")
-);
+--     CONSTRAINT "players_pkey" PRIMARY KEY ("username")
+-- );
 
--- CreateTable
-CREATE TABLE "reminders" (
-    "id" SERIAL NOT NULL,
-    "guild_id" VARCHAR(30),
-    "channel_id" VARCHAR(30) NOT NULL,
-    "user_id" VARCHAR(30) NOT NULL,
-    "message_contents" VARCHAR(128) NOT NULL,
-    "reminder_time" BIGINT NOT NULL,
-    "interaction_reply_id" VARCHAR(30),
+-- -- CreateTable
+-- CREATE TABLE "reminders" (
+--     "id" SERIAL NOT NULL,
+--     "guild_id" VARCHAR(30),
+--     "channel_id" VARCHAR(30) NOT NULL,
+--     "user_id" VARCHAR(30) NOT NULL,
+--     "message_contents" VARCHAR(128) NOT NULL,
+--     "reminder_time" BIGINT NOT NULL,
+--     "interaction_reply_id" VARCHAR(30),
 
-    CONSTRAINT "reminders_pkey" PRIMARY KEY ("id")
-);
+--     CONSTRAINT "reminders_pkey" PRIMARY KEY ("id")
+-- );
 
--- CreateTable
-CREATE TABLE "tracked_instances" (
-    "raid_id" VARCHAR(8) NOT NULL,
+-- -- CreateTable
+-- CREATE TABLE "tracked_instances" (
+--     "raid_id" VARCHAR(8) NOT NULL,
 
-    CONSTRAINT "tracked_instances_pkey" PRIMARY KEY ("raid_id")
-);
+--     CONSTRAINT "tracked_instances_pkey" PRIMARY KEY ("raid_id")
+-- );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "migrations_name_key" ON "migrations"("name");
