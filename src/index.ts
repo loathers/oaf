190b331e8568424ec3f8f9d98f5120e1caf576d0
Sam Gaus <sam@gaus.co.uk>
2023-05-01 10:33:47 +0100

    Do not attempt to load test files as commands

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function* walk(dir: string): AsyncGenerator<string> {
 async function loadSlashCommands() {
   const commandsPath = path.join(__dirname, "commands");
   for await (const filePath of walk(commandsPath)) {
-    if (!/\/[^_][^\/]*.(ts|js)$/.test(filePath)) continue;
+    if (!/\/[^_][^\/]*(?<!\.test)\.(ts|js)$/.test(filePath)) continue;
     let handled = false;
     const command: CommandHandler | ModalHandler = await import(filePath);
     if ("data" in command) {
