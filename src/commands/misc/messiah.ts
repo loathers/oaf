279aca51a20ba94efc394350c975071703cb16e7
Sam Gaus <sam@gaus.co.uk>
2023-05-03 10:26:25 +0100

    Remove some old fashioned requires that TS can now tell us off about

--- src/commands/misc/messiah.ts
+++ src/commands/misc/messiah.ts
@@ -1,6 +1,5 @@
 import { AttachmentBuilder, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
-import sharp = require("sharp");
+import sharp from "sharp";
 
 export const data = new SlashCommandBuilder()
   .setName("messiah")
