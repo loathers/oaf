12d48d7d46b6635ee64038daa5fd9917ee477f6a
horrible little slime <69secret69email69@gmail.com>
2023-05-05 13:00:00 -0400

    use Number.isNaN instead of a regex

--- src/commands/kol/whois.ts
+++ src/commands/kol/whois.ts
@@ -57,7 +57,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     }
 
     playerIdentifier = knownPlayer.playerId || knownPlayer.username;
-  } else if (input.match(/\d+/)) {
+  } else if (!Number.isNaN(+input)) {
     playerIdentifier = Number(input)
   } else if (validUsername(input)) {
     playerIdentifier = input;
