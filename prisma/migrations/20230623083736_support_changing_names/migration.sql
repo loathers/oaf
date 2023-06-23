06562f769048a1c767195cb43872d160d3b3ae77
Sam Gaus <sam@gaus.co.uk>
2023-06-23 10:08:53 +0100

    Fix migration

--- prisma/migrations/20230623083736_support_changing_names/migration.sql
+++ prisma/migrations/20230623083736_support_changing_names/migration.sql
@@ -1,8 +1,5 @@
 -- DropConstraint
 ALTER TABLE "Player" DROP CONSTRAINT "Player_username_key";
 
--- DropIndex
-DROP INDEX "Player_username_key";
-
 -- AlterTable
 ALTER TABLE "Player" RENAME COLUMN "username" TO "playerName";
