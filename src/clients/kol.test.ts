5857ccceca4932eefbe42ad902f1aae6e00edded
Sam Gaus <sam@gaus.co.uk>
2023-06-23 11:34:38 +0100

    Add kol client test

---
+++ src/clients/kol.test.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { afterAll, afterEach, beforeAll, expect, test, vi } from "vitest";
+
+import { respondWithFixture } from "../testUtils";
+import { kolClient } from "./kol";
+
+vi.mock("axios");
+
+beforeAll(() => {
+  kolClient.mockLoggedIn = true;
+});
+
+afterAll(() => {
+  kolClient.mockLoggedIn = false;
+});
+
+afterEach(() => {
+  vi.mocked(axios).mockReset();
+});
+
+function expectNotNull<T>(value: T | null): asserts value is T {
+  expect(value).not.toBeNull();
+}
+
+test("Can search for a player by name", async () => {
+  vi.mocked(axios).mockResolvedValueOnce(
+    await respondWithFixture(__dirname, "searchplayer_mad_carew.html")
+  );
+
+  const player = await kolClient.getPartialPlayerFromName("mad carew");
+
+  expectNotNull(player);
+
+  expect(player.id).toBe(263717);
+  expect(player.level).toBe(16);
+  expect(player.class).toBe("Sauceror");
+  // Learns correct capitalisation
+  expect(player.name).toBe("Mad Carew");
+});
