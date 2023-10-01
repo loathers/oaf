5b1e7363789cda4c5440679a91860a875a619113
horrible little slime <69secret69email69@gmail.com>
2023-09-30 20:25:01 -0400

    calculate reverseLep correctly for hobo monkey

--- src/commands/kol/reverseleprechaun.ts
+++ src/commands/kol/reverseleprechaun.ts
@@ -19,6 +19,6 @@ export function execute(interaction: ChatInputCommandInteraction) {
   interaction.reply(
     `To get ${meatDrop}% meat drop from a leprechaun, ` +
       `it should weigh at least ${toWeight(meatDrop / 2).toFixed(1)} lbs, ` +
-      `or be a Hobo Monkey that weighs at least ${toWeight(meatDrop / 2).toFixed(1)} lbs.`,
+      `or be a Hobo Monkey that weighs at least ${toWeight(meatDrop / 2, 1.25).toFixed(1)} lbs.`,
   );
 }
