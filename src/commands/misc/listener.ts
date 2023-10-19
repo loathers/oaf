6b1e16cff16795c55eb8d4abeb701e407eed14b7
Sam Gaus <sam@gaus.co.uk>
2023-10-19 13:27:12 +0100

    Add listener role to any members with zero roles total

--- src/commands/misc/listener.ts
+++ src/commands/misc/listener.ts
@@ -9,13 +9,38 @@ async function onJoinServer(member: GuildMember) {
   const role = await member.guild.roles.fetch(config.LISTENER_ROLE_ID);
   if (!role) {
     await discordClient.alert(
-      `Failed to grant member ${member.displayName} the Listener role`,
+      `Failed to grant member ${member.displayName} the Listener role because role doesn't exist`,
     );
     return;
   }
   await member.roles.add(role, "Member added upon joining server");
 }
 
+async function sync() {
+  const guild = await discordClient.guilds.fetch(config.GUILD_ID);
+
+  const role = await guild.roles.fetch(config.LISTENER_ROLE_ID);
+
+  if (!role) {
+    await discordClient.alert("Listener role doesn't exist");
+    return;
+  }
+
+  const results = await Promise.all(
+    guild.members.cache
+      .filter((member) => member.roles.cache.size < 1)
+      .map((member) => member.roles.add(role)),
+  );
+
+  if (results.length === 0) return;
+
+  await discordClient.alert(
+    `Added listener role to ${results.length} listener(s) who had no roles.`,
+  );
+}
+
 export async function init() {
   discordClient.on(Events.GuildMemberAdd, onJoinServer);
+
+  await sync();
 }
