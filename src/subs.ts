3bac0ca147ac052d6d926ffd9956bb8392ee6feb
neil <78829653+horrible-little-slime@users.noreply.github.com>
2023-10-31 16:54:24 -0400

    make oaf remind people not to talk about farming in the iotm channel (#165)
    * make oaf remind people not to talk about farming in the iotm channel
    
    * actually make a thread
    
    * month printing
    
    * use `date-fns`
    
    * further streamlining

--- src/subs.ts
+++ src/subs.ts
@@ -1,7 +1,20 @@
-import { roleMention } from "discord.js";
+import { add, closestTo, format } from "date-fns";
+import { ThreadAutoArchiveDuration, roleMention } from "discord.js";
 
 import { discordClient } from "./clients/discord.js";
 import { config } from "./config.js";
+
+function determineIotmMonthYear() {
+  const today = new Date();
+
+  format(
+    closestTo(today, [
+      add(today, { months: 1 }).setDate(1),
+      add(today, { months: -1 }).setDate(1),
+    ]) ?? today,
+    "LLLL y",
+  );
+}
 
 export async function rollSubs() {
   const guild = await discordClient.guilds.fetch(config.GUILD_ID);
@@ -17,12 +30,20 @@ export async function rollSubs() {
   const subRollEmoji =
     guild.emojis.cache.find((e) => e.name === "subsRolling") ?? "";
 
-  await iotmChannel.send({
+  const message = await iotmChannel.send({
     content: `🚨${subRollEmoji} Attention ${roleMention(
       config.SUBSCRIBER_ROLE_ID,
-    )}! This is an automated message to let you know that subscriptions are now rolling ${subRollEmoji}🚨`,
+    )}! This is an automated message to let you know that subscriptions are now rolling ${subRollEmoji}🚨
+    
+Feel free to discuss spading and speed strats here in the main channel; speculation about farming strategy should be done in the thread.`,
     allowedMentions: {
       roles: [config.SUBSCRIBER_ROLE_ID],
     },
+  });
+
+  await message.startThread({
+    name: `Farming Discussion for ${determineIotmMonthYear()} IotM`,
+    autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+    reason: `to discuss & speculate about farming strategy for the ${determineIotmMonthYear()} IotM`,
   });
 }
