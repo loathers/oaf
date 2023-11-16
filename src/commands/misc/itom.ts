281b36341b0f8fbe9ff045566b1cc715c16d8482
horrible little slime <69secret69email69@gmail.com>
2023-11-16 12:46:03 -0500

    wikipedia clowns

--- src/commands/misc/itom.ts
+++ src/commands/misc/itom.ts
@@ -1,8 +1,31 @@
+import axios from "axios";
 import { DiscordAPIError, Events, Message } from "discord.js";
 
 import { discordClient } from "../../clients/discord.js";
 
 const ITOM_MATCHER = /(?:\W|^)(itoms?)(?:[^a-z]|$)/i;
+
+async function getClownName(): Promise<string> {
+  const rawClownList: string = await axios(`https://en.wikipedia.org/api.php`, {
+    params: {
+      action: "query",
+      list: "categorymembers",
+      cmtitle: "Category:American_clowns",
+      cmprop: "title",
+      cmnamespace: 0,
+      format: "json",
+      cmlimit: 100,
+    },
+  });
+
+  const clownList: { ns: number; title: string }[] =
+    JSON.parse(rawClownList).query.categorymembers;
+  const filteredList = clownList.filter(
+    ({ title }) => title !== "John Wayne Gacy",
+  );
+
+  return filteredList[Math.floor(Math.random() * filteredList.length)].title;
+}
 
 async function onMessage(message: Message) {
   if (message.author.bot) return;
@@ -20,6 +43,10 @@ async function onMessage(message: Message) {
           `Tried to :minusone: a message containing the forbidden string by ${message.author}; received error ${error}.`,
         ));
       }
+
+      const clownName = await getClownName();
+
+      return void (await member.setNickname(clownName));
     }
   }
 }
