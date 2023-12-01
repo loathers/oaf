f3241e59fa48732f46fbf006c14f5f4aa816695c
horrible little slime <69secret69email69@gmail.com>
2023-12-01 10:41:10 -0500

    let extended team members rename oaf

---
+++ src/commands/misc/rename.ts
@@ -0,0 +1,58 @@
+import {
+  ChatInputCommandInteraction,
+  GuildMemberRoleManager,
+  SlashCommandBuilder,
+} from "discord.js";
+
+import { discordClient } from "../../clients/discord.js";
+import { config } from "../../config.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("rename")
+  .setDescription("Renames our beloved O.A.F.")
+  .addStringOption((o) =>
+    o
+      .setName("name")
+      .setDescription("Our beloved O.A.F.'s new name")
+      .setRequired(true),
+  );
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+  const member = interaction.member;
+
+  if (!member) {
+    interaction.reply({
+      content: "You have to perform this action from within a Guild.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  const roleManager = member.roles as GuildMemberRoleManager;
+
+  if (!roleManager.cache.has(config.EXTENDED_TEAM_ROLE_ID)) {
+    interaction.reply({
+      content: "You are not permitted to rename me, your beloved O.A.F.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  const name = interaction.options.getString("name", true);
+  await discordClient.user?.setUsername(name);
+
+  if (discordClient.user?.username === name) {
+    return void (await interaction.reply({
+      content: `Done! My name is now ${name}`,
+      ephemeral: true,
+    }));
+  } else {
+    discordClient.alert(
+      `${interaction.user.username} tried to change O.A.F.'s name to ${name}, but failed!`,
+    );
+    return void (await interaction.reply({
+      content: "Sorry, that command didn't work. My name is still my name.",
+      ephemeral: true,
+    }));
+  }
+}
