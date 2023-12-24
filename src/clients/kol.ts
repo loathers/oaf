e5672ebab9c5623c920027032ff85824e24c45b6
horrible little slime <69secret69email69@gmail.com>
2023-12-23 22:54:38 -0500

    use `/listenon`, because vinnyb is the truest king of ASS

--- src/clients/kol.ts
+++ src/clients/kol.ts
@@ -288,6 +288,7 @@ export class KoLClient extends (EventEmitter as new () => TypedEmitter<Events>) 
   async startChatBot() {
     if (this.chatBotStarted) return;
     await this.useChatMacro("/join talkie");
+    await this.useChatMacro("/listenon crimbo");
     this.loopChatBot();
     this.chatBotStarted = true;
   }
