802a7214df14c9bded44076ea8ef4cd857dad9a9
neil <78829653+horrible-little-slime@users.noreply.github.com>
2024-04-01 12:27:07 -0400

    Aen's April Fools idea (#194)
    * Aen's April Fools idea
    
    * refine

---
+++ src/commands/misc/april.ts
@@ -0,0 +1,21 @@
+import { milliseconds } from "date-fns";
+import { Events, TextChannel } from "discord.js";
+
+import { discordClient } from "../../clients/discord.js";
+
+const CHECK_DURATION: Duration = { seconds: 10 };
+async function startTyping(): Promise<void> {
+  const today = new Date();
+  if (today.getMonth() === 3 && today.getDate() === 1) {
+    discordClient.channels.cache.each(
+      (channel) => channel instanceof TextChannel && channel.sendTyping(),
+    );
+  }
+}
+
+export async function init() {
+  discordClient.on(
+    Events.ClientReady,
+    () => void setInterval(startTyping, milliseconds(CHECK_DURATION)),
+  );
+}
