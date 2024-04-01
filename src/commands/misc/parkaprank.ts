900e1e677e90accda6b72fe9f73e5bd583417516
neil <78829653+horrible-little-slime@users.noreply.github.com>
2024-04-01 17:13:38 -0400

    Remove manual trigger (#199)
    * remove manual trigger for parka
    
    * final fixes
    
    * fix typo

--- src/commands/misc/parkaprank.ts
+++ src/commands/misc/parkaprank.ts
@@ -1,11 +1,5 @@
 import { differenceInMinutes, milliseconds } from "date-fns";
-import {
-  ChatInputCommandInteraction,
-  Events,
-  GuildMemberRoleManager,
-  SlashCommandBuilder,
-  blockQuote,
-} from "discord.js";
+import { Events, ThreadAutoArchiveDuration, blockQuote } from "discord.js";
 import { dedent } from "ts-dedent";
 
 import { discordClient } from "../../clients/discord.js";
@@ -23,43 +17,21 @@ async function announceParka() {
     return;
   }
 
-  return void (await announcementChannel.send({
+  const message = await announcementChannel.send({
     content: dedent`
       New announcement posted to KoL chat!
       ${blockQuote(
         "A new announcement has been posted: Welcome to the Jurassic Parka, which is April's Item-of-the-Month and now available in Mr. Store.",
       )}
     `,
+  });
+
+  return void (await message.startThread({
+    name: `Discussion for announcement`,
+    autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
   }));
 }
 
-export const data = new SlashCommandBuilder()
-  .setName("backdoorparka")
-  .setDescription("Don't worry about it");
-
-export async function execute(interaction: ChatInputCommandInteraction) {
-  const member = interaction.member;
-
-  if (!member) {
-    interaction.reply({
-      content: "You have to perform this action from within a Guild.",
-      ephemeral: true,
-    });
-    return;
-  }
-
-  const roleManager = member.roles as GuildMemberRoleManager;
-
-  if (!roleManager.cache.has(config.EXTENDED_TEAM_ROLE_ID)) {
-    interaction.reply({
-      content: "You are not permitted to do secret things.",
-      ephemeral: true,
-    });
-    return;
-  }
-
-  return await announceParka();
-}
 async function checkAnnounceParka() {
   if (!discordClient.parka) return;
   if (differenceInMinutes(new Date(), discordClient.parka) >= 30) {
