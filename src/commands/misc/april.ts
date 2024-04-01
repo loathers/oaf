a83a7babca9a8616d94d0cb057cbb5f2aa1e2eff
horrible little slime <69secret69email69@gmail.com>
2024-04-01 12:58:49 -0400

    limit channels for april typing

--- src/commands/misc/april.ts
+++ src/commands/misc/april.ts
@@ -3,18 +3,22 @@ import { Events, TextChannel } from "discord.js";
 
 import { discordClient } from "../../clients/discord.js";
 
+const APRIL_CHANNEL_IDS =
+  "1207483178931982346,533092807473102869,581155825385472001,581155825385472001,1161394735085256735,938767927606403072,756649296774037567,781270875080622080,781092861093085195,534709227260870656466659737010831360";
+
 const CHECK_DURATION: Duration = { seconds: 10 };
 async function startTyping(): Promise<void> {
   const today = new Date();
   if (today.getMonth() === 3 && today.getDate() === 1) {
-    discordClient.channels.cache.each(
-      (channel) =>
-        channel instanceof TextChannel &&
-        channel
-          .permissionsFor(discordClient.member ?? "")
-          ?.has("SendMessages") &&
-        channel.sendTyping(),
-    );
+    APRIL_CHANNEL_IDS.split(",")
+      .map((id) => discordClient.channels.cache.get(id))
+      .filter((ch): ch is TextChannel =>
+        Boolean(
+          ch instanceof TextChannel &&
+            discordClient.member?.permissionsIn(ch).has("SendMessages"),
+        ),
+      )
+      .forEach((channel) => channel.sendTyping());
   }
 }
 
