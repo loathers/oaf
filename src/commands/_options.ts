001e977ff4cdddadf1fe732c6b53e9cfe6733a84
Sam Gaus <sam@gaus.co.uk>
2024-04-12 16:56:17 +0100

    Pull out item autocomplete offer into a util file

---
+++ src/commands/_options.ts
@@ -0,0 +1,27 @@
+import { SlashCommandIntegerOption } from "discord.js";
+
+import { wikiClient } from "../clients/wiki.js";
+
+export const itemOption =
+  (required = true) =>
+  (option: SlashCommandIntegerOption) =>
+    option
+      .setName("item")
+      .setDescription(
+        "Select an item from the autocomplete or supply an item id",
+      )
+      .setAutocomplete(true)
+      .setRequired(required);
+
+export const itemAutocomplete = (value: string) =>
+  wikiClient.items
+    .map(({ name, id }) => ({ name, value: id }))
+    .filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()))
+    .toSorted((a, b) =>
+      a.name.toLowerCase() === value.toLowerCase()
+        ? -1
+        : b.name.toLowerCase() === value.toLowerCase()
+          ? 1
+          : 0,
+    )
+    .slice(0, 25);
