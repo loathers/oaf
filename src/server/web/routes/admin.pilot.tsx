3848cdc9a603f89cec828ce3dd2ea58645f6865a
Sam Gaus <sam@gaus.co.uk>
2024-04-19 13:53:18 +0100

    Add emoji to admin pilot feature

--- src/server/web/routes/admin.pilot.tsx
+++ src/server/web/routes/admin.pilot.tsx
@@ -5,6 +5,12 @@ import {
   Button,
   FormControl,
   FormLabel,
+  HStack,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Select,
   Stack,
   Textarea,
@@ -18,19 +24,32 @@ import {
 import { messageLink } from "discord.js";
 import React, { useEffect, useRef } from "react";
 
+import { DiscordClient } from "../../../clients/discord";
 import { authenticator } from "../auth.server";
+
+async function fetchChannels(discordClient: DiscordClient) {
+  if (!discordClient.guild) return [];
+
+  return [...discordClient.guild.channels.cache.values()]
+    .filter((c) => c.isTextBased())
+    .map((c) => ({ name: c.name, id: c.id }));
+}
+
+async function fetchEmoji(discordClient: DiscordClient) {
+  if (!discordClient.guild) return [];
+
+  return [...discordClient.guild.emojis.cache.values()].map((e) => ({
+    name: e.name,
+    id: e.id,
+    url: e.url,
+  }));
+}
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const { discordClient } = context;
-
-  if (!discordClient.guild) return json({ channels: [] });
-
-  const channels = [...discordClient.guild.channels.cache.values()];
-
   return json({
-    channels: channels
-      .filter((c) => c.isTextBased())
-      .map((c) => ({ name: c.name, id: c.id })),
+    channels: await fetchChannels(discordClient),
+    emoji: await fetchEmoji(discordClient),
   });
 }
 
@@ -66,10 +85,17 @@ export async function action({ request, context }: ActionFunctionArgs) {
 
 export default function Pilot() {
   const messageInput = useRef<HTMLTextAreaElement>(null);
-  const { channels } = useLoaderData<typeof loader>();
+  const { channels, emoji } = useLoaderData<typeof loader>();
   const fetcher = useFetcher<typeof action>();
 
   const success = fetcher.data?.success;
+
+  function addEmoji({ name, id }: (typeof emoji)[number]) {
+    const el = messageInput.current;
+    if (!el) return;
+    el.value += `${el.value.endsWith(" ") ? "" : " "}<:${name}:${id}>`;
+    return false;
+  }
 
   useEffect(() => {
     if (success && messageInput.current) messageInput.current.value = "";
@@ -96,7 +122,40 @@ export default function Pilot() {
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Message</FormLabel>
-          <Textarea name="content" ref={messageInput} />
+          <HStack alignItems="start">
+            <Textarea name="content" ref={messageInput} />
+            <Menu>
+              <MenuButton
+                as={Button}
+                size="xs"
+                type="button"
+                title="Emoji Picker"
+              >
+                🙂
+              </MenuButton>
+              <MenuList
+                maxWidth="80px"
+                maxHeight="200px"
+                overflowY="scroll"
+                display="flex"
+                flexWrap="wrap"
+              >
+                {emoji.map((e) => (
+                  <MenuItem
+                    key={e.id}
+                    display="inline"
+                    width="auto"
+                    paddingInlineStart={1}
+                    paddingInlineEnd={1}
+                    type="button"
+                    onClick={() => addEmoji(e)}
+                  >
+                    <Image width="30px" title={e.name || ""} src={e.url} />
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </HStack>
         </FormControl>
         <Button type="submit">Send</Button>
       </Stack>
