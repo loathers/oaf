a60bf7ffe81f82b3f77de687135c58341d4980f2
Sam Gaus <sam@gaus.co.uk>
2025-08-24 14:23:17 +0100

    Report alerts from the eggnet

---
+++ packages/oaf/src/server/eggnet.ts
@@ -0,0 +1,37 @@
+import { createEmbed, discordClient } from "../clients/discord.js";
+import { wikiClient } from "../clients/wiki.js";
+import { config } from "../config.js";
+
+type Details = {
+  monsterId: number;
+};
+
+export async function eggnet({ monsterId }: Details) {
+  const guild = await discordClient.guilds.fetch(config.GUILD_ID);
+  const iotmChannel = guild?.channels.cache.get(config.IOTM_CHANNEL_ID);
+
+  if (!iotmChannel?.isTextBased()) {
+    await discordClient.alert(
+      "Someone has tried to hit a Samsara webhook but the guild or unrestricted channel are incorrectly configured",
+    );
+    throw new Error("Something is configured wrong");
+  }
+
+  const monster = wikiClient.monsters.find((i) => i.id === monsterId);
+
+  if (!monster) {
+    await discordClient.alert(
+      `Eggnet reported monster with id ${monsterId} was completed, but they're not in our database`,
+    );
+    throw new Error("Something is configured wrong");
+  }
+
+  const embed = createEmbed();
+  embed.setTitle(monster.name).setURL(await wikiClient.getWikiLink(monster));
+  await monster.addToEmbed(embed);
+
+  await iotmChannel.send({
+    content: `Huh? ${monster.name} came out of its egg 🥚! New monster available in the Mimic DNA Bank`,
+    embeds: [embed],
+  });
+}
