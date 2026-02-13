e7e058100254f887d4a6c3cc9461d31df127df09
Sam Gaus <sam@gaus.co.uk>
2026-02-13 16:27:35 +0000

    Add advanced kmail parsing to kol.js

--- packages/kol.js/src/index.ts
+++ packages/kol.js/src/index.ts
@@ -2,5 +2,5 @@ export { Player } from "./Player.js";
 export { Client, type MallPrice } from "./Client.js";
 
 export { resolveKoLImage } from "./utils/utils.js";
-export type { KoLMessage } from "./utils/kmail.js";
+export type { KmailItem, KoLMessage } from "./utils/kmail.js";
 export type { SubboardInfo } from "./utils/leaderboard.js";
