cd09e648a534394435df6dc4c12dc62220818b90
Sam Gaus <sam@gaus.co.uk>
2026-05-08 22:15:28 +0100

    Fix dataOfLoathing.test.ts fixture for v3 Monster shape

--- packages/oaf/src/clients/dataOfLoathing.test.ts
+++ packages/oaf/src/clients/dataOfLoathing.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument */
 import { describe, expect, test } from "vitest";
 
 import { Monster } from "../things/Monster.js";
@@ -12,12 +13,8 @@ describe("Wiki link", () => {
       attack: "100",
       boss: true,
       defence: "100",
-      element: null,
       drippy: false,
-      elementalAttack: "HOT",
-      elementalDefence: null,
       elementalResistance: "0",
-      experience: null,
       free: false,
       ghost: false,
       groupSize: 1,
@@ -26,23 +23,20 @@ describe("Wiki link", () => {
       initiative: "100",
       itemBlockChance: 0,
       lucky: false,
-      manuel: null,
       meat: 0,
-      meatExpression: null,
       monsterLevelMultiplier: "1",
       name: "The Hermit",
       nobanish: false,
       nocopy: true,
       nomanuel: true,
-      nodeId: "WyJtb25zdGVycyIsMTc4MV0=",
       nowander: false,
       nowish: false,
       phylum: "dude",
       physicalResistance: "0",
-      poison: null,
       scaling: "0",
       scalingFloor: "0",
       scalingCap: "0",
+      skeleton: false,
       skillBlockChance: 0,
       snake: false,
       sprinkles: ["0", "0"],
@@ -52,23 +46,10 @@ describe("Wiki link", () => {
       wanderer: false,
       wiki: "Hermit (monster)",
       wish: false,
-      monsterDropsByMonster: {
-        nodes: [],
-      },
-      nativeMonstersByMonster: {
-        nodes: [],
-        edges: [],
-        pageInfo: {
-          hasNextPage: false,
-          hasPreviousPage: false,
-          startCursor: null,
-          endCursor: null,
-          __typename: "PageInfo",
-        },
-        totalCount: 0,
-        __typename: "NativeMonstersConnection",
-      },
-    });
+      zombie: false,
+      drops: { getItems: () => [] },
+      nativeLocations: { getItems: () => [] },
+    } as any);
 
     const link = dataOfLoathingClient.getWikiLink(monster);
 
